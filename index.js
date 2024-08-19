7ea8daf629d793ee275e70227e0f73fdf3c7e692
Sourav Jaiswal <jaiswalsourav5@gmail.com>
2024-08-19 16:43:09 +0530

    updated v

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
-const simpleGit = require('simple-git');
-const random  = require('random');
-const FILE_PATH = './data.json';
+const simpleGit = require('simple-git')();
 
-const makeCommit = n => {
-    if(n===0) return simpleGit().push();
-    const x = random.int(0,54);
-    const y = random.int(0,6);
+(async () => {
+    const random = await import('random');
+    const FILE_PATH = './data.json';
 
-    const DATE = moment().subtract(1,'y').add(1, 'd').add(x,'w').add(y,'d').format();
+    // Set the start date (joining date) and end date (August 2023)
+    const startDate = moment('2022-01-01'); // Replace with actual joining date
+    const endDate = moment('2023-07-30');
 
-    const data = {
-        date : DATE
-    }
-     console.log(DATE);
-    jsonfile.writeFile(FILE_PATH, data, ()=> {
-        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE},
-            makeCommit.bind(this, --n));
-    
-    });
-    
-}
+    // Calculate the total number of days between the start and end dates
+    const totalDays = endDate.diff(startDate, 'days');
 
-makeCommit(100);
+    const makeCommit = async (n) => {
+        if (n === 0) return simpleGit.push();
 
+        // Calculate a random number of days to add to the start date
+        const randomDays = random.default.int(0, totalDays);
 
+        // Generate the commit date
+        const DATE = moment(startDate).add(randomDays, 'days').format();
 
+        const data = { date: DATE };
 
+        console.log(DATE);
+
+        await jsonfile.writeFile(FILE_PATH, data);
+        await simpleGit.add([FILE_PATH]).commit(DATE, { '--date': DATE });
+        makeCommit(n - 1);
+    };
+
+    makeCommit(200); // Number of commits
+})();
